dde7ab0d2848fa5aa915b83dd51bcecd7821d5a4
ionif <aionkov@gmail.com>
2020-03-10 12:12:12 -0500

    GTex preprocessing, was doing it using dicts but Matt showed me a much easier way with Numpy and pandas

--- recount2/parseRecount.py
+++ recount2/parseRecount.py
@@ -1,8 +1,36 @@
 import json
+import numpy as np
+import pandas as pd
+
 """
-parses tsv file
+parses tsv file using column headers as keys
 """
-def parseTSV(path, index):
+def parseTSV(path):
+    dict = {}
+
+    with open(path) as file:
+        line_idx = 0
+        for line in file:
+            #get headers in a list
+            if line_idx == 0:
+                lsplit = line.split("\t")
+                headers = [i.strip() for i in lsplit]
+            #get each item in a dict with the index as the key
+            if line_idx != 0:
+                lsplit = line.split("\t")
+                for item in lsplit:
+                    #check if key exists
+                    if lsplit.index(item) in dict:
+                        dict[lsplit.index(item)].append(item.strip())
+                    else:
+                        dict[lsplit.index(item)] = []
+            line_idx += 1
+    return headers, dict
+
+"""
+parses column of tsv file
+"""
+def parseTSVCol(path, index):
     list = []
 
     with open(path) as file:
@@ -26,7 +54,13 @@ def toJson(path, dict):
         file.close()
 
 #to get list of runs in order
-toJson("runs_in_order.json", parseTSV("SRP012682.tsv", 3))
+#toJson("runs_in_order.json", parseTSVCol("SRP012682.tsv", 3))
 
 #to get list of all genes
-toJson("gene_names.json", parseTSV("counts_gene.tsv", 9662))
+#toJson("gene_names.json", parseTSVCol("counts_gene.tsv", 9662))
+
+#to get numpy array with rows as samples and columns as genes, each element is a count
+df = pd.read_csv("counts_gene.tsv", sep="\t")
+df.set_index("gene_id")
+x = np.array(df)
+np.save("recount2.npy", x.T)
